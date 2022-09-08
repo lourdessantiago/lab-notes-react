8818a45f1b4bdd5a51dd7bc512552c5a010e9c96
Lourdes Santiago <lourdes.santiago.palacios@gmail.com>
2022-09-08 00:39:02 -0500

    Compoenete home
    Maquetacion del componente home

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import imgMancha from '../assets/mancha.png'
+import imgFlower from '../assets/flower.png'
+
+export function Home() {
+
+    return(
+        
+        <>
+        <img src={imgMancha} alt="mancha" className='img-left-sup'/>
+        <img src={imgFlower} alt="flower" className='img-rigth-sup'/>
+        <h1 className='title-home'>Anotando</h1>
+        <h2 className='desc-home'> Crea </h2>
+        <h2 className='desc-home'> notas </h2>
+        <h2 className='desc-home' > fácil </h2>
+        <h2 className='desc-home' > y </h2>
+        <h2 className='desc-home' > rápido </h2>
+        <button type='submit' className='btn-google'> icono gGoogle </button>
+
+        </>
+    )
+    
+}
